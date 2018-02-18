2bb340a8194d2d984a99bc0be507dfb9f82b05e4
Leocaliban <leocaliban@gmail.com>
2018-02-18 19:17:04 -0300

    Cria as Entidades do Model (Lancamento, Pessoa, Tipo)

---
+++ src/com/leocaliban/financas/model/Pessoa.java
@@ -0,0 +1,35 @@
+package com.leocaliban.financas.model;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer codigo;
+	private String nome;
+	
+	public Pessoa() {
+		
+	}
+	
+	public Pessoa(Integer codigo, String nome) {
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
